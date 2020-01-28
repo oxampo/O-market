1e2af071408e401944569cf008f56b52da67f075
Carlos Perez <c.perez@correo.unimet.edu.ve>
2020-01-27 23:46:26 -0400

    MySQL Connection

--- omarket/omarket/settings.py
+++ omarket/omarket/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'omarket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'omarketdb',
+        'USER': 'root',
+        'PASSWORD': 'toor', 
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    },
 }
 
 
