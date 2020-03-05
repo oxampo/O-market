a3a02493a08c3d407be0c51817db40d1920b85d3
Carlos Perez <c.perez@correo.unimet.edu.ve>
2020-03-05 04:53:47 -0400

    Casi listos todos los datos del form de venta (quiero cargarlos todos en un dict)

--- omarket/modelsUTILS/views.py
+++ omarket/modelsUTILS/views.py
@@ -10,25 +10,30 @@ def VentasMain(request):
     return render(request,'ventas/main.html')
 
 
-
 def Vender(request):
-
+    data = {}
     if request.method=='POST':
         #validación para ver si el usuario existe; si existe, se compra
-       
-        cedula = request.POST.get('cedula')
+        if request.POST.get('cedula'):
+            cedula = request.POST.get('cedula')
 
-        if not int(cedula)>0:
-                return render(request,'ventas/vender.html',{'error':'cédula no válida ({} es negativo)'.format(cedula)})
-        try:
-            profile = Profile.objects.get(cedula=int(cedula))
-        except:      
-            return render(request,'ventas/vender.html',{'error':'no existe perfil/usuario de cédula {}. Debe registrarlo'.format(cedula)})
+            if not int(cedula)>0:
+                    return render(request,'ventas/vender.html',{'error':'cédula no válida ({} es negativo)'.format(cedula)})
+            try:
+                profile = Profile.objects.get(cedula=int(cedula))
+            except:      
+                return render(request,'ventas/vender.html',{'error':'no existe perfil/usuario de cédula {}. Debe registrarlo'.format(cedula)})
+            
+            user = User.objects.get(profile=profile)
+            data['nombre'] =user.first_name
+            data['apellido'] =user.last_name
+            data['cedula'] =profile.cedula
+
+            return render(request,'ventas/vender.html', {'data':data,'ready':True})
         
-        user = User.objects.get(profile=profile)
-        data = {'nombre':user.first_name, 'apellido':user.last_name,'cedula':profile.cedula}
-
-        return render(request,'ventas/vender.html', {'data':data,'ready':True})
+        elif request.POST.get('pago'):
+            data['pago']=request.POST['pago']
+            print(data)
         
         
     return render(request,'ventas/vender.html')
