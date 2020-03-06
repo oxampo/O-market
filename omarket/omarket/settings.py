7678dc4d801eb63569336dbba425dc5c2fec822f
dap305 <da.prieto@correo.unimet.edu.ve>
2020-03-06 02:30:17 -0400

    nice

--- omarket/omarket/settings.py
+++ omarket/omarket/settings.py
@@ -89,9 +89,9 @@ WSGI_APPLICATION = 'omarket.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'omarket9',
-        'USER': 'root',
-        'PASSWORD': '', 
+        'NAME': 'omarketgus',
+        'USER': 'dan',
+        'PASSWORD': '123', 
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
